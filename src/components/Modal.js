3ae5dfac05501ddd5eb850bbac221536045933b5
Nicolas Le Borgne <nicolas.leborgne@me.com>
2021-01-18 09:10:06 +0100

    🐛 FIX: fix mechanism text overflow on modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -63,7 +63,9 @@ class Modal extends React.Component {
                       Mechanism
                     </dt>
                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                      {this.props.data.mechanism}
+                      <span className="block break-words overflow-clip overflow-hidden">
+                        {this.props.data.mechanism}
+                      </span>
                     </dd>
                   </div>
                   <div className="text-left bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
